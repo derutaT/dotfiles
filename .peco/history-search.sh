6924247a520df380c20cc663265e5b6ea73f845c
derutaT <derutaT@gmail.com>
2016-02-17 20:31:31 +0900

    set peco query as console command buffer

--- .peco/history-search.sh
+++ .peco/history-search.sh
@@ -1,6 +1,6 @@
 # ref http://qiita.com/shepabashi/items/f2bc2be37a31df49bca5
 function peco-history-selection() {
-    BUFFER=`history -n 1 | tail -r  | awk '!a[$0]++' | peco`
+    BUFFER=`history -n 1 | tail -r  | awk '!a[$0]++' | peco --query "$BUFFER"`
     CURSOR=$#BUFFER
     zle reset-prompt
 }
