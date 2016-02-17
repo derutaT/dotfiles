bd9f6c5c6e756d45d1042e8c96b21297c247c736
derutaT <derutaT@gmail.com>
2016-02-17 20:28:40 +0900

    add history search function

---
+++ .peco/history-search.sh
@@ -0,0 +1,9 @@
+# ref http://qiita.com/shepabashi/items/f2bc2be37a31df49bca5
+function peco-history-selection() {
+    BUFFER=`history -n 1 | tail -r  | awk '!a[$0]++' | peco`
+    CURSOR=$#BUFFER
+    zle reset-prompt
+}
+
+zle -N peco-history-selection
+bindkey '^R' peco-history-selection
